402b53247c3c9cbf17c9d1e09e2bc57949b979e3
西行寺幽幽子 <1732285681@qq.com>
2019-05-22 16:27:28 +0800

    首次提交

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './home/Home'
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Home/>
+    </div>
+  );
+}
+
+export default App;
